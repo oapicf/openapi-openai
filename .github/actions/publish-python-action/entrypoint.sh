e2bed80f71109a76c8bd8412a70aab54165f2d31
Cliffano Subagio <cliffano@gmail.com>
2026-08-16 16:30:00 +1000

    Remove GH action files, superseded by workflows command-docker-action.

--- .github/actions/publish-python-action/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-make clean deps init-spec generate-primary build-python test-python
-
-rm -f ~/.pypirc && touch ~/.pypirc
-echo "[pypi]" > ~/.pypirc
-echo "username = __token__" >> ~/.pypirc
-echo "password = ${PYPI_TOKEN}" >> ~/.pypirc
-
-make publish-python
